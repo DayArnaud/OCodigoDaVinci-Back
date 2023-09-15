5cd849ee611815fc4fc5c43c7c74ed5caa02b1a1
DayaneArnaud <dayanearnaud@gmail.com>
2023-09-15 17:22:49 -0300

    [del]:delete file `server.js`

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 require("dotenv").config();
-const app = require("./server");
+const express = require("express");
+const knex = require("./connection");
+const cors = require("cors");
+const app = express();
 
-// app.get("/", async (req, res) => {
-//   return res.json("API está ok!");
-// });
+app.use(express.json());
+app.use(cors());
+// app.use(routes);
+
+// const routes = require("./routes/router");
+
+app.get("/", async (req, res) => {
+  try {
+    const users = await knex("users");
+    return res.json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Server error" });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${port}`);
 });
+
+module.exports = app;
