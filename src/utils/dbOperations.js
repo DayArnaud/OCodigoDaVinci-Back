1edf849269bc73092d73534242408d8390b4ea06
DayaneArnaud <dayanearnaud@gmail.com>
2023-09-15 14:18:45 -0300

    [setup]:initial deployment setup and configuration

---
+++ src/utils/dbOperations.js
@@ -0,0 +1,1 @@
+// Neste arquivo irão métodos que farão consultas ou modificações no BD, como checar se email, cpf, novo usuário, novo cliente e alguns outros verbos HTTP. Este arquivo dá suporte ao controlador.
