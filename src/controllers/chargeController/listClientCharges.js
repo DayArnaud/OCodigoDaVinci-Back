4526a3e824e99d2f3109bbde66abfc1857e919ba
DayaneArnaud <dayanearnaud@gmail.com>
2023-09-21 15:35:09 -0300

    [FEAT] controller `listClientCharges.js`

---
+++ src/controllers/chargeController/listClientCharges.js
@@ -0,0 +1,30 @@
+const knex = require("../../connection");
+
+const HTTP_NOT_FOUND = 404;
+const HTTP_SUCCESS = 200;
+const HTTP_SERVER_ERROR = 500;
+
+const listClientCharges = async (req, res) => {
+  const { clientId } = req.params;
+
+  try {
+    const charges = await knex("charges")
+      .where({ client_id: clientId })
+      .select("*");
+
+    if (!charges.length) {
+      return res
+        .status(HTTP_NOT_FOUND)
+        .json({ message: "Nenhuma cobrança encontrada para este cliente" });
+    }
+
+    return res.status(HTTP_SUCCESS).json(charges);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(HTTP_SERVER_ERROR)
+      .json({ message: "Erro interno do servidor" });
+  }
+};
+
+module.exports = listClientCharges;
